872bedc3e4e3441621255607b8c50c47bad085b0
Ny2fly <103384751+Ny2fly@users.noreply.github.com>
2022-09-27 20:09:48 -0400

    Update complex.js

--- complex.js
+++ complex.js
@@ -1,37 +1,4 @@
-let greeting='hello world';
-
-Let thenumber; variable
-thenumber=10; data
-thenumber=3.44; data
-
-//bollian//
-
-let addbutter= true;
-
-let lightison=false;
-
-let a= 
-let a2= 
-let b=
-    
-    //aray- instead of listing things separately just make them int he same container called an aray. you can put all types of data into an array//
-    
-    
-    let luckynumbers=[
-        '1',
-         '3',
-         '27',
-         '89',
-        ]
-        
-console.log(luckynumbers[3]);
-
-    
-    
-
-
-
-
-
-
-
+let bike={
+    color:blue
+    brand:trek
+}
